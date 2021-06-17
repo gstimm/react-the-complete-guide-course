a93725acc8c02847673355d903b1917cc286c9c6
gstimm <contato.gabrieltimm@gmail.com>
2021-06-17 15:31:56 -0300

    first commit

---
+++ 01-first-project/src/components/Expenses/index.jsx
@@ -0,0 +1,33 @@
+import React, { useState } from 'react';
+
+import ExpensesList from '../ExpensesList';
+import Card from '../Card';
+import ExpensesFilter from '../ExpensesFilter';
+import ExpensesChart from '../ExpensesChart';
+
+import './style.css';
+
+const Expense = (props) => {
+  const [filteredYear, setFilteredYear] = useState('2021');
+
+  const filterChangeHandler = (selectedYear) => {
+    setFilteredYear(selectedYear);
+  };
+
+  const filteredExpenses = props.items.filter((expense) => {
+    return expense.date.getFullYear().toString() === filteredYear;
+  });
+
+  return (
+    <Card className="expenses">
+      <ExpensesFilter
+        selected={filteredYear}
+        onChangeFilter={filterChangeHandler}
+      />
+      <ExpensesChart expenses={filteredExpenses} />
+      <ExpensesList items={filteredExpenses} />
+    </Card>
+  );
+};
+
+export default Expense;
