6c944cc9ef0c83c974b027631e8f3698064bf657
gstimm <contato.gabrieltimm@gmail.com>
2021-06-22 16:22:24 -0300

    feat: add context to cart and badge number update

--- 08-food-order-app-project/src/components/Layout/HeaderCartButton/index.js
+++ 08-food-order-app-project/src/components/Layout/HeaderCartButton/index.js
@@ -1,20 +1,25 @@
-import React from 'react';
+import { useContext } from 'react';
 
 import CartIcon from '../../Cart/CartIcon';
-
+import CartContext from '../../../store/cart-context';
 import styles from './styles.module.css';
 
 const HeaderCartButton = (props) => {
+  const cartCtx = useContext(CartContext);
+
+  const numberOfCartItems = cartCtx.items.reduce((currentNumber, item) => {
+    return currentNumber + item.amount;
+  }, 0);
+
   return (
     <button className={styles.button} onClick={props.onClick}>
       <span className={styles.icon}>
         <CartIcon />
       </span>
       <span>Your Cart</span>
-      <span className={styles.badge}>3</span>
-
+      <span className={styles.badge}>{numberOfCartItems}</span>
     </button>
-  )
-}
+  );
+};
 
 export default HeaderCartButton;
