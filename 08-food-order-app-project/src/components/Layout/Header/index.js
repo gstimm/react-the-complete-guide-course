6b5b8b76c91c28797d4f8758d111df0e7e8a08ba
gstimm <contato.gabrieltimm@gmail.com>
2021-06-22 14:13:13 -0300

    feat: add some initial components

---
+++ 08-food-order-app-project/src/components/Layout/Header/index.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+import HeaderCartButton from '../HeaderCartButton';
+
+import mealsImage from '../../../assets/meals.jpg';
+import styles from './styles.module.css';
+
+const Header = (props) => {
+  return (
+    <>
+      <header className={styles.header}>
+        <h1>ReactMeals</h1>
+        <HeaderCartButton />
+      </header>
+      <div className={styles['main-image']}>
+        <img src={mealsImage} alt="HeaderImage" />
+      </div>
+    </>
+  );
+};
+
+export default Header;
