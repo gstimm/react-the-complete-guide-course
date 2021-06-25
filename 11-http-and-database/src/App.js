3101eb6efc97e0bc303be2e944c6fac4c40ad511
gstimm <contato.gabrieltimm@gmail.com>
2021-06-25 14:27:14 -0300

    feat: add Firebase and AddMovie Component

--- 11-http-and-database/src/App.js
+++ 11-http-and-database/src/App.js
@@ -1,6 +1,8 @@
 import React, { useState, useEffect, useCallback } from 'react';
 
+import AddMovie from './components/AddMovie';
 import MoviesList from './components/MoviesList';
+
 import './App.css';
 
 function App() {
@@ -12,23 +14,36 @@ function App() {
     setIsLoading(true);
     setError(null);
     try {
-      const response = await fetch('https://swapi.dev/api/films/');
+      const response = await fetch(
+        'https://react-http-71352-default-rtdb.firebaseio.com/movies.json'
+      );
 
       if (!response.ok) {
         throw new Error('Something went wrong!');
       }
 
       const data = await response.json();
+      
+      const loadedMovies = [];
 
-      const transformedMovies = data.results.map((movieData) => {
-        return {
-          id: movieData.episode_id,
-          title: movieData.title,
-          openingText: movieData.opening_crawl,
-          releaseDate: movieData.release_date,
-        };
-      });
-      setMovies(transformedMovies);
+      for (const key in data) {
+        loadedMovies.push({
+          id: key,
+          title: data[key].title,
+          openingText: data[key].openingText,
+          releaseDate: data[key].releaseDate,
+        })
+      }
+
+      // const transformedMovies = data.results.map((movieData) => {
+      //   return {
+      //     id: movieData.episode_id,
+      //     title: movieData.title,
+      //     openingText: movieData.opening_crawl,
+      //     releaseDate: movieData.release_date,
+      //   };
+      // });
+      setMovies(loadedMovies);
     } catch (error) {
       setError(error.message);
     }
@@ -39,6 +54,26 @@ function App() {
   useEffect(() => {
     fetchMoviesHandler();
   }, [fetchMoviesHandler]);
+
+  const addMovieHandler = useCallback(async (movie) => {
+    try {
+      const response = await fetch(
+        'https://react-http-71352-default-rtdb.firebaseio.com/movies.json',
+        {
+          method: 'POST',
+          body: JSON.stringify(movie),
+          headers: {
+            'Content-Type': 'application/json',
+          },
+        }
+      );
+      const data = await response.json();
+
+      console.log(data);
+    } catch (error) {
+      setError(error.message);
+    }
+  }, []);
 
   let content = <p>Found no movies.</p>;
 
@@ -56,6 +91,9 @@ function App() {
 
   return (
     <React.Fragment>
+      <section>
+        <AddMovie onAddMovie={addMovieHandler} />
+      </section>
       <section>
         <button onClick={fetchMoviesHandler}>Fetch Movies</button>
       </section>
