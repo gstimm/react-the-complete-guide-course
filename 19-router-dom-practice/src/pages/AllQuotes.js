27398e162aa461bef2e568271723eda271fdb00f
gstimm <contato.gabrieltimm@gmail.com>
2021-07-02 12:36:28 -0300

    feat: add data to details page

--- 19-router-dom-practice/src/pages/AllQuotes.js
+++ 19-router-dom-practice/src/pages/AllQuotes.js
@@ -1,4 +1,11 @@
+import QuoteList from '../components/quotes/QuoteList';
+
+const DUMMY_QUOTES = [
+  { id: 'q1', author: 'Gabriel', text: 'Learning React.' },
+  { id: 'q2', author: 'Timm', text: 'Anything.' },
+];
+
 const AllQuotes = () => {
-  return <h1>All Quotes</h1>;
+  return <QuoteList quotes={DUMMY_QUOTES} />;
 };
 export default AllQuotes;
--- 19-router-dom-practice/src/pages/QuoteDetail.js
+++ 19-router-dom-practice/src/pages/QuoteDetail.js
@@ -1,4 +1,29 @@
+import { useParams, Route } from 'react-router-dom';
+
+import Comments from '../components/comments/Comments';
+import HighlightedQuote from '../components/quotes/HighlightedQuote';
+
+const DUMMY_QUOTES = [
+  { id: 'q1', author: 'Gabriel', text: 'Learning React.' },
+  { id: 'q2', author: 'Timm', text: 'Anything.' },
+];
+
 const QuoteDetail = () => {
-  return <h1>Quote Detail</h1>;
+  const params = useParams();
+
+  const quote = DUMMY_QUOTES.find((quote) => quote.id === params.quoteId);
+
+  if (!quote) {
+    return <p>No Quote Found!</p>;
+  }
+
+  return (
+    <>
+      <HighlightedQuote text={quote.text} author={quote.author} />
+      <Route path={`/quotes/${params.quoteId}/comments`}>
+        <Comments />
+      </Route>
+    </>
+  );
 };
 export default QuoteDetail;
