1187522bed9cd727903d34fc7d2855779545206f
gstimm <contato.gabrieltimm@gmail.com>
2021-07-07 13:24:13 -0300

    feat: hooks with typescript done

---
+++ ts-hooks/src/App.tsx
@@ -0,0 +1,38 @@
+import React, {
+  useState,
+  useEffect,
+  useMemo,
+  useCallback,
+  useRef,
+  useContext,
+  useImperativeHandle,
+  useReducer,
+} from 'react';
+
+interface User {
+  name: string;
+  login: string;
+  avatar_url: string;
+}
+
+const App: React.FC = () => {
+  const inputRef = useRef<HTMLInputElement>(null);
+  const [users, setUsers] = useState<[User]>();
+
+  const names = useMemo(
+    () => users?.map((user) => user.name).join(', '),
+    [users]
+  );
+
+  const greeting = useCallback((user: User) => alert(`Hello ${user.name}`), []);
+
+  inputRef.current?.focus();
+
+  return (
+    <form action="">
+      <input type="text" ref={inputRef} />
+    </form>
+  )
+};
+
+export default App;
